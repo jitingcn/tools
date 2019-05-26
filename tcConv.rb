95876357272fcadc77cf2ae38786b3cf559fd041
Jiting <jiting@jtcat.com>
2019-05-26 16:53:13 +0800

    fix bug

--- tcConv.rb
+++ tcConv.rb
@@ -2,6 +2,7 @@
 
 require 'optparse'
 
+# defined option
 OptionParser.new do |opts|
   opts.banner = 'Matroska Timecodes Version Converter'
 
@@ -19,6 +20,8 @@ OptionParser.new do |opts|
 
   $help = opts
 end.parse!
+
+# check and read option
 options = {format: $format, input: $input, output: $output}
 unless options[:input]
   puts $help
@@ -29,15 +32,21 @@ unless File.exist?(options[:input])
   puts '[error]: input file not exist.'
   exit
 end
+
+# process data 
+$meta = []
+pattern = /^(?<name>[#]\s+.+\S)\s+(?<format>\S+)$/x
 tc = File.open(options[:input]).each_line.to_a.map{ |line|
-  if line =~ /# timecode format+/
-    options[:format]="tc1"
+  if line =~ /format/
+    $meta = line.match(pattern)
+    if $meta["format"] == "v1"
+      options[:format]="v1"
+    else
+      options[:format]="v2"
+    end
     next
   end
   if line =~ /Assume+/
-    options[:format]="tc2"
-    # puts 'tc1 to tc2 function broken. exit.'
-    # exit
     next
   end
   line = line.strip.split","
@@ -49,13 +58,13 @@ tc = File.open(options[:input]).each_line.to_a.map{ |line|
 }
 #pp options, tc[0..4]
 out = []
-if options[:format] == "tc1"
+if options[:format] == "v2"
   tc.shift
   fps = []
   (tc.length-1).times do |i|
     fps.append(1000/(tc[i+1]-tc[i]).to_f)
   end
-  out.append("# timecode format v1","Assume #{sprintf("%.6f", fps.max)}")
+  out.append("#{$meta["name"]} v1","Assume #{sprintf("%.6f", fps.max)}")
   index = 0
   fps.each do |item|
     item = sprintf("%.6f", item)
@@ -74,7 +83,7 @@ if options[:format] == "tc1"
   end
   lastest_item=out[-1].split(",")
   out[-1].gsub!(/,.+,/, ",#{lastest_item[1]},"=>",#{index},")
-elsif options[:format] == "tc2"
+elsif options[:format] == "v1"
   def inter(num)
     num_s = num.to_s.split"."
     case num_s[1]
@@ -87,10 +96,10 @@ elsif options[:format] == "tc2"
     end
   end
   tc.shift(2)
-  out.append("# timecode format v2")
+  out.append("#{$meta["name"]} v2")
   tc.each do |item|
     lastest=out[-1]
-    if lastest =~ /# timecode format+/
+    if lastest =~ /format/
       lastest = 0
       out.append(0)
     end
@@ -114,7 +123,7 @@ elsif options[:format] == "tc2"
 end
 #pp out[-5..-1]
 unless options[:output]
-  options[:output] = options[:input].split(".")[0..-2].join(".")+"-#{options[:format]}.txt"
+  options[:output] = options[:input].split(".")[0..-2].join(".")+"-#{$meta["format"] == 'v1' ? "v2" : "v1"}.txt"
 end
 if File.exist?(options[:output])
   print "File #{options[:output]} already exists. Overwrite ? [y/N] "
@@ -125,13 +134,13 @@ if File.exist?(options[:output])
 end
 File.open(options[:output], mode="w") do |f|
   case options[:format]
-  when "tc1"
+  when "v2"
     out.each do |line|
       f.write(line+"\n")
     end
-  when "tc2"
+  when "v1"
     out.each do |line|
-      if line =~ /# timecode format+/
+      if line =~ /format/
         f.write(line+"\n")
         next
       end
